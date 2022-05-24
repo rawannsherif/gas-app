c6957151b63ffb4e79f13f00a9897db6e5fa8b07
rawann <rawannsherif@gmail.com>
2022-05-24 16:02:05 +0200

    testing API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   
+    // Constructor 
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+   
+    // ComponentDidMount is used to
+    // execute the code 
+    componentDidMount() {
+        fetch(
+"https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div> ;
+   
+        return (
+        <div className = "App">
+            <h1> Fetch data from an api in react </h1>  {
+                items.map((item) => ( 
+                <ol key = { item.id } >
+                    User_Name: { item.username }, 
+                    Full_Name: { item.name }, 
+                    User_Email: { item.email } 
+                    </ol>
+                ))
+            }
+        </div>
+    );
 }
-
+}
+   
 export default App;
