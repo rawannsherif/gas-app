e77969ae13c2be636f2bd008a4e2f5cacfb97766
rawann <rawannsherif@gmail.com>
2022-05-26 13:54:20 +0200

    changed API

--- src/App.js
+++ src/App.js
@@ -6,38 +6,44 @@ class App extends React.Component {
     constructor(props) {
         super(props);
    
-        this.state = {
+        this.state = ({
             items: [],
             DataisLoaded: false
-        };
+        })
     }
    
     // ComponentDidMount is used to
     // execute the code 
     componentDidMount() {
         fetch(
-"https://jsonplaceholder.typicode.com/users")
+"https://www.etherchain.org/api/gasPriceOracle")
             .then((res) => res.json())
             .then((json) => {
+              console.log(json)
                 this.setState({
-                    items: json,
-                    DataisLoaded: true
-                });
+                     items: json,
+                     DataisLoaded: true
+                 });
+                
             })
     }
     render() {
         const { DataisLoaded, items } = this.state;
+        //const itemss =  Object.keys(items);
+        //console.log(Object.keys(itemss));
+
         if (!DataisLoaded) return <div>
             <h1> Pleses wait some time.... </h1> </div> ;
    
         return (
+
         <div className = "App">
             <h1> Fetch data from an api in react </h1>  {
-                items.map((item) => ( 
-                <ol key = { item.id } >
-                    User_Name: { item.username }, 
-                    Full_Name: { item.name }, 
-                    User_Email: { item.email } 
+                Object.keys(items).map(item => (
+                <ol>
+                    Safe_Low: { item.safeLow }, 
+                    Standard: { item.standard }, 
+                    Fast: { item.fast }
                     </ol>
                 ))
             }
